5dab669c7fd2e530d2bd2796868f13b5b05ec768
Cristhian892 <cristhian.jimenezcampos@ucr.ac.cr>
2026-04-23 14:38:30 -0600

    [200~

---
+++ src/py/Tensor Broadcasting Basics.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def broadcast_ops(X: np.ndarray, b: np.ndarray, w: np.ndarray): 
+    suma = (X + b) 
+    multipicador = w.reshape(-1, 1)
+    Y = suma * multipicador
+    return Y
+    
+
+    
+
+#ejemplo 1
+X = np.array([[1,2],
+              [3,4]])
+b = np.array([10,20])
+w = np.array([0.5,2])
+
+aplicar = broadcast_ops(X, b, w)
+print(aplicar)
