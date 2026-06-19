449ead0007bcc89feb8a01d81b1fc4c7459eaa80
unknown <jmalfarosaldana@gmail.com>
2026-06-18 20:18:36 -0600

    agrega ejercicio 10 B2 (SGD Optimizer)

---
+++ src/py/SGD_Optimizer.py
@@ -0,0 +1,61 @@
+
+#10) SGD Optimizer
+
+#Queremos actualizar el w utilizando SGD
+
+import numpy as np
+
+def sgd_step(w,dw,lr):
+    nuevo_w = w - lr*dw
+    
+    return nuevo_w
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
